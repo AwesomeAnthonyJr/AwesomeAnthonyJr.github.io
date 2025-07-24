5de36ed931a1ab581f595b1ba37bee8c57f14d4b
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2025-07-24 05:29:41 -0400

    mediocre mobile functionality

--- scripts/test_scripts.js
+++ scripts/test_scripts.js
@@ -3,6 +3,7 @@ const items = Array.from(track.children);
 const prevButton = document.querySelector('.carousel-button-prev');
 const nextButton = document.querySelector('.carousel-button-next');
 const expandButton = document.querySelector('.carousel-button-expand');
+const mobileExpandButton = document.querySelector('.carousel-button-mobile');
 const expandImage = document.querySelector('.carousel-expand-image');
 const root = document.documentElement;
 const background = document.querySelector('.carousel-background');
@@ -16,6 +17,7 @@ let mobileView = getComputedStyle(root).getPropertyValue('--mobile-view').trim()
 //console.log(caroGap);
 
 let expanded = false;
+let mobileExpanded = false;
 let currentIndex = 2;
 let true_caro_gap = 0;
 let caro_offset = window.innerWidth * 0.25;
@@ -69,6 +71,10 @@ function carouselPrev() {
   currentIndex--;
   updatePosition();
 
+  if (mobileExpanded){
+    unexpandMobileDesc();
+  }
+
   background.style.opacity = "0";
   background.addEventListener('transitionend', () => {
     updateCaroBackground();
@@ -85,6 +91,10 @@ function carouselPrev() {
 function carouselNext() {
   currentIndex++;
   updatePosition();
+  
+  if (mobileExpanded){
+    unexpandMobileDesc();
+  }
 
   background.style.opacity = "0";
   background.addEventListener('transitionend', () => {
@@ -341,8 +351,6 @@ function unexpandCaroItem() {
   }, { once: true });
 }
 
-
-
 async function type(item, text, timeout) {
   //title.textContent = "";
   console.log("the length is", text.length)
@@ -356,6 +364,59 @@ async function type(item, text, timeout) {
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function updateMobileDescription() {
+  if (mobileView == 0) return;
+  const descriptionBoss = document.querySelector('.carousel-description-mobile');
+  const description = document.querySelector('.carousel-description-mobile > div');
+
+  description.innerHTML = '';
+  switch (currentIndex) {
+    case 2:
+      description.innerHTML = `<h2>XCVB</h2> <h3>May 25th 2025</h3> <br> <p>XCVB is a roguelike deckbuilder / 2D platformer mashup originally created in for Untitled Game Jam #110. You play as a lone gambler with a deck of magical cards who ascend's Starface's tower in order to reclaim the $5 they are owed. Featuring 16 different movement cards (for active abilities), and 24 different badges (for passive perks), There are countless combinations to discover. The enemies in the tower get stronger as you climb, but on your own terms, as you get to decide if they become faster or stronger each floor. You can play XCVB for free today on it's <a href="https://leaflight-studios.itch.io/xcvb">itch.io</a> page, or <a href="https://leaflight-studios.itch.io/">learn more about it here.</a></p> <br> <h3>Created By: Anthony D. Salsbury, Owen Hickman, & Samuel Radulski</h3>`;
+      break;
+    case 3:
+      description.innerHTML = `<h2>Kill Protocol</h2> <h3>October 11th 2024</h3> <br> <p>Kill Protocol is a unique two-color shooter created originally for 1-BIT JAM #4. Set in a future where clones are easily accessible, lives have little meaning, you must pay back $4500 of debt to regain your freedom, and taking assasination jobs to do so. If you have good credit you can spend some on the black market to enhance your loadout. All guns in this dystopian future are linked to ID codes, and can be morphed into others, while reloading directly charges your (in game) bank account. Kill Protocol can be played with a free download from it's<a href="https://leaflight-studios.itch.io/kill-protocol"> itch.io </a>page, or<a href="https://leaflight-studios.itch.io/"> learn more about it here.</a></p> <br> <h3>Created By: Anthony D. Salsbury, Jonathan Schultz, Justin Grant, George Dietritch, Owen Hickman, & Samuel Radulski</h3>`;
+      break;
+    case 4:
+      description.innerHTML = `<h2>Mage Hand</h2> <h3>July 31st 2024</h3> <br> <p>Mage Hand is a knowledge-based exploration game originally created for Pirate Software Game Jam #15. You are a magician who has lost your memory, but retained your ability to cast spells via different hand signs. Explore out of the prison tower and find your way out of the mansion, with secret routes and legendary wizards, reclaim your memory and master some of the 40 different spells to defeat your captor and set yourself free. You can play Mage Hand for free directly from it's <a href="https://leaflight-studios.itch.io/mage-hand">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury, Josh "BB" Bang, Willow Bradshaw</h3>`;
+      break;
+    case 5:
+      description.innerHTML = `<h2>goblin knight</h2> <h3>May 11th 2024</h3> <br> <p>goblin knight is an experimental 1st person sword fighting game initially created for the Game Idea Jam 2024. Play as an explorer who has gone too deep in the dark, meeting the goblin knight. It displays an interesting idea with it's aiming-based melee combat. You can download goblin knight for free from it's <a href="https://leaflight-studios.itch.io/goblin-knight">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
+      break;
+    case 6:
+      description.innerHTML = `<h2>Wyrm Canyon</h2> <h3>April 20th 2024</h3> <br> <p>Wyrm Canyon on the Pink and Purple Planet is an arcade-style, two-color game originally made for 1-BIT JAM #3. Pilot your sand tank with a unique control scheme and use it's compass to locate the Wyrms of the canyon, all before striking with your smylsknife on foot. If Pink and Purple isn't your style then you can pick a different color palette in the settings as well. You can play it for free directly from Wyrm Canyon's <a href="https://leaflight-studios.itch.io/wyrm-canyon">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
+      break;
+    case 7:
+      description.innerHTML = `<h2>SHAHARAZON</h2> <h3>March 14th 2024</h3> <br> <p>SHAHARAZON is a first person "grenadier" set in the dark future of 2424, originally created for Acerola Jam 0. Play through a simulation of the war of the future as a cyborg super-soldier, trained for combat in grenade-only situations. You can play SHAHARAZON for free by downloading it from it's <a href="https://leaflight-studios.itch.io/agj0project">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
+      break;
+    default:
+      console.log("unexpected!");  
+  }
+
+  await sleep(10);
+  descriptionBoss.style.height = description.scrollHeight + 30 + 'px';
+  descriptionBoss.style.borderWidth = "4px";
+}
+
+function hideMobileDescription() {
+  const descriptionBoss = document.querySelector('.carousel-description-mobile');
+  //const description = document.querySelector('.carousel-description-mobile > div');
+  descriptionBoss.style.height = '0';
+  descriptionBoss.addEventListener('transitionend', () => {
+    descriptionBoss.style.borderWidth = "0px";
+  }, { once: true });
+}
+
+function expandMobileDesc() {
+  updateMobileDescription();
+  mobileExpanded = true;
+}
+
+function unexpandMobileDesc() {
+  hideMobileDescription();
+  mobileExpanded = false;
 }
 
 prevButton.addEventListener('click', () => {
@@ -378,10 +439,26 @@ expandButton.addEventListener('click', () => {
   //expanded = !expanded;
 });
 
+mobileExpandButton.addEventListener('click', () => {
+  if (mobileView == 0) return;
+  if (mobileExpanded){
+    unexpandMobileDesc();
+  } else {
+    expandMobileDesc();
+  }
+});
+
 window.addEventListener('resize', () => {
   caro_offset = window.innerWidth * 0.25;
   updateTrueGap();
   updatePosition();
+  if (expanded && window.innerWidth <= 950){
+    unexpandCaroItem();
+    expanded = false;
+  } else if (mobileExpanded && window.innerWidth >= 950){
+    unexpandMobileDesc();
+    mobileExpanded = false;
+  }
 });
 //console.log(items.length)
 updateCaroBackground();
