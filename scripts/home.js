e2839801110916ab223b45da19447dd4d37aa2be
Anthony D. Salsbury <anthony.salsbury@outlook.com>
2025-10-01 14:30:04 -0400

    testing DUI in carousel

--- scripts/home.js
+++ scripts/home.js
@@ -127,26 +127,30 @@ function updateCaroImage() {
   }
   switch (caro_index) {
     case 2:
-      console.log("XCVB");
+      console.log("DUI");
       expandImage.style.backgroundImage = "url(" + "styles/images/XCVB_display_" + url_ender + ")";
       break;
     case 3:
+      console.log("XCVB");
+      expandImage.style.backgroundImage = "url(" + "styles/images/XCVB_display_" + url_ender + ")";
+      break;
+    case 4:
       console.log("KILLPROTOCOL");
       expandImage.style.backgroundImage = "url(" + "styles/images/KillProtocol_display_" + url_ender + ")";
       break;
-    case 4:
+    case 5:
       console.log("MAGEHAND");
       expandImage.style.backgroundImage = "url(" + "styles/images/MageHand_display_" + url_ender + ")";
       break;
-    case 5:
+    case 6:
       console.log("GOBLINKNIGHT");
       expandImage.style.backgroundImage = "url(" + "styles/images/goblinknight_display_" + url_ender + ")";
       break;
-    case 6:
+    case 7:
       console.log("WYRMCANYON");
       expandImage.style.backgroundImage = "url(" + "styles/images/WyrmCanyon_display_" + url_ender + ")";
       break;
-    case 7:
+    case 8:
       console.log("SHAHARAZON");
       expandImage.style.backgroundImage = "url(" + "styles/images/SHAHARAZON_display_" + url_ender + ")";
       break;
@@ -164,13 +168,13 @@ function updateCaroBackground() {
       background.style.backgroundImage = "url(styles/images/SHAHARAZON_background.png)";
       break;
     case 2:
-      background.style.backgroundImage = "url(styles/images/XCVB_background.png)";
+      background.style.backgroundImage = "url()";
       break;
     case 3:
-      background.style.backgroundImage = "url(styles/images/KillProtocol_background.png)";
+      background.style.backgroundImage = "url(styles/images/XCVB_background.png)";
       break;
     case 4:
-      background.style.backgroundImage = "url()";
+      background.style.backgroundImage = "url(styles/images/KillProtocol_background.png)";
       break;
     case 5:
       background.style.backgroundImage = "url()";
@@ -179,11 +183,14 @@ function updateCaroBackground() {
       background.style.backgroundImage = "url()";
       break;
     case 7:
-      background.style.backgroundImage = "url(styles/images/SHAHARAZON_background.png)";
+      background.style.backgroundImage = "url()";
       break;
     case 8:
+      background.style.backgroundImage = "url(styles/images/SHAHARAZON_background.png)";
+      break;
+    case 9:
       //match case 2!
-      background.style.backgroundImage = "url(styles/images/XCVB_background.png)";
+      background.style.backgroundImage = "url()";
       break;
     default:
       console.log("unexpected!");
@@ -200,6 +207,14 @@ function updateCaroDescription() {
   descriptionCR.innerHTML = '';
   switch (caro_index) {
     case 2:
+      descriptionBG.className =  'carousel-description-background-DUI';
+      descriptionTX.className =  'carousel-description-text-DUI';
+      descriptionCR.className =  'carousel-description-credit-DUI';
+      
+      descriptionTX.innerHTML = `<p>DUI: Drive Until Impact is a cool game</p>`;
+      descriptionCR.innerHTML = `<p>Created By: Anthony D. Salsbury,<br> Others </p>`
+      break;
+    case 3:
       descriptionBG.className =  'carousel-description-background-XCVB';
       descriptionTX.className =  'carousel-description-text-XCVB';
       descriptionCR.className =  'carousel-description-credit-XCVB';
@@ -207,7 +222,7 @@ function updateCaroDescription() {
       descriptionTX.innerHTML = `<p>XCVB is a roguelike deckbuilder / 2D platformer mashup originally created in for Untitled Game Jam #110. You play as a lone gambler with a deck of magical cards who ascend's Starface's tower in order to reclaim the $5 they are owed. Featuring 16 different movement cards (for active abilities), and 24 different badges (for passive perks), There are countless combinations to discover. The enemies in the tower get stronger as you climb, but on your own terms, as you get to decide if they become faster or stronger each floor. You can play XCVB for free today on its <a href="https://leaflight-studios.itch.io/xcvb">itch.io</a> page, or <a href="/games/xcvb">learn more about it here.</a></p>`;
       descriptionCR.innerHTML = `<p>Created By: Anthony D. Salsbury,<br> Owen Hickman, & Samuel Radulski</p>`
       break;
-    case 3:
+    case 4:
       descriptionBG.className =  'carousel-description-background-KillProtocol';
       descriptionTX.className =  'carousel-description-text-KillProtocol';
       descriptionCR.className =  'carousel-description-credit-KillProtocol';
@@ -217,7 +232,7 @@ function updateCaroDescription() {
       
       
       break;
-    case 4:
+    case 5:
       descriptionBG.className =  'carousel-description-background-MageHand';
       descriptionTX.className =  'carousel-description-text-MageHand';
       descriptionCR.className =  'carousel-description-credit-MageHand';
@@ -226,7 +241,7 @@ function updateCaroDescription() {
       descriptionCR.innerHTML = `<p>Created By: Anthony D. Salsbury, <br> Josh "BB" Bang, Willow Bradshaw</p>`;
 
       break;
-    case 5:
+    case 6:
       descriptionBG.className =  'carousel-description-background-goblinknight';
       descriptionTX.className =  'carousel-description-text-goblinknight';
       descriptionCR.className =  'carousel-description-credit-goblinknight';
@@ -234,7 +249,7 @@ function updateCaroDescription() {
       descriptionTX.innerHTML = `<p>goblin knight is an experimental 1st person sword fighting game initially created for the Game Idea Jam 2024. Play as an explorer who has gone too deep in the dark, meeting the goblin knight. It displays an interesting idea with it's aiming-based melee combat. You can download goblin knight for free from its <a href="https://leaflight-studios.itch.io/goblin-knight">itch.io</a> page, or <a href="/games/goblin-knight">learn more here</a></p>`;
       descriptionCR.innerHTML = `<p>created by: anthony d. salsbury</p>`;
       break;
-    case 6:
+    case 7:
       descriptionBG.className =  'carousel-description-background-WyrmCanyon';
       descriptionTX.className =  'carousel-description-text-WyrmCanyon';
       descriptionCR.className =  'carousel-description-credit-WyrmCanyon';
@@ -242,7 +257,7 @@ function updateCaroDescription() {
       descriptionTX.innerHTML = `<p>Wyrm Canyon on the Pink and Purple Planet is an arcade-style, two-color game originally made for 1-BIT JAM #3. Pilot your sand tank with a unique control scheme and use it's compass to locate the Wyrms of the canyon, all before striking with your smylsknife on foot. If Pink and Purple isn't your style then you can pick a different color palette in the settings as well. You can play it for free directly from Wyrm Canyon's <a href="https://leaflight-studios.itch.io/wyrm-canyon">itch.io</a> page, or <a href="/games/wyrm-canyon">learn more here</a></p>`;
       descriptionCR.innerHTML = `<p><span>Created By:</span><br> Anthony D. Salsbury</p>`;
       break;
-    case 7:
+    case 8:
       descriptionBG.className =  'carousel-description-background-SHAHARAZON';
       descriptionTX.className =  'carousel-description-text-SHAHARAZON';
       descriptionCR.className =  'carousel-description-credit-SHAHARAZON';
@@ -260,21 +275,24 @@ async function updateTitle() {
   if (mobile_view == 1) return;
   switch (caro_index) {
     case 2:
-      await type(title, "XCVB", 20);
+      await type(title, "DUI: Drive Until Impact", 20);
       break;
     case 3:
-      await type(title, "Kill Protocol", 20);
+      await type(title, "XCVB", 20);
       break;
     case 4:
-      await type(title, "Mage Hand", 20);
+      await type(title, "Kill Protocol", 20);
       break;
     case 5:
-      await type(title, "goblin knight", 20);
+      await type(title, "Mage Hand", 20);
       break;
     case 6:
-      await type(title, "Wyrm Canyon", 20);
+      await type(title, "goblin knight", 20);
       break;
     case 7:
+      await type(title, "Wyrm Canyon", 20);
+      break;
+    case 8:
       await type(title, "SHAHARAZON", 20);
       break;
     default:
@@ -286,21 +304,24 @@ function updateRelease() {
   if (mobile_view == 1) return;
   switch (caro_index) {
     case 2:
-      type(release, "May 25th 2025", 20);
+      type(release, "September 28th 2025", 20);
       break;
     case 3:
-      type(release, "October 11th 2024", 20);
+      type(release, "May 25th 2025", 20);
       break;
     case 4:
-      type(release, "July 31st 2024", 20);
+      type(release, "October 11th 2024", 20);
       break;
     case 5:
-      type(release, "May 11th 2024", 20);
+      type(release, "July 31st 2024", 20);
       break;
     case 6:
-      type(release, "April 20th 2024", 20);
+      type(release, "May 11th 2024", 20);
       break;
     case 7:
+      type(release, "April 20th 2024", 20);
+      break;
+    case 8:
       type(release, "March 14th 2024", 20);
       break;
     default:
@@ -379,22 +400,25 @@ async function updateMobileDescription() {
 
   description.innerHTML = '';
   switch (caro_index) {
-    case 2:
-      description.innerHTML = `<h2>XCVB</h2> <h3>May 25th 2025</h3> <br> <p>XCVB is a roguelike deckbuilder / 2D platformer mashup originally created in for Untitled Game Jam #110. You play as a lone gambler with a deck of magical cards who ascend's Starface's tower in order to reclaim the $5 they are owed. Featuring 16 different movement cards (for active abilities), and 24 different badges (for passive perks), There are countless combinations to discover. The enemies in the tower get stronger as you climb, but on your own terms, as you get to decide if they become faster or stronger each floor. You can play XCVB for free today on its <a href="https://leaflight-studios.itch.io/xcvb">itch.io</a> page, or <a href="https://leaflight-studios.itch.io/">learn more about it here.</a></p> <br> <h3>Created By: Anthony D. Salsbury, Owen Hickman, & Samuel Radulski</h3>`;
+     case 2:
+      description.innerHTML = `<h2>DUI: Drive Until Impact</h2> <h3>September 28th 2025</h3> <br> <p>DUI desc</p> <br> <h3>Created By: Anthony D. Salsbury, Owen Hickman, & Samuel Radulski and more</h3>`;
       break;
     case 3:
-      description.innerHTML = `<h2>Kill Protocol</h2> <h3>October 11th 2024</h3> <br> <p>Kill Protocol is a unique two-color shooter created originally for 1-BIT JAM #4. Set in a future where clones are easily accessible, lives have little meaning, you must pay back $4500 of debt to regain your freedom, and taking assasination jobs to do so. If you have good credit you can spend some on the black market to enhance your loadout. All guns in this dystopian future are linked to ID codes, and can be morphed into others, while reloading directly charges your (in game) bank account. Kill Protocol can be played with a free download from its<a href="https://leaflight-studios.itch.io/kill-protocol"> itch.io </a>page, or<a href="https://leaflight-studios.itch.io/"> learn more about it here.</a></p> <br> <h3>Created By: Anthony D. Salsbury, Jonathan Schultz, Justin Grant, George Dietritch, Owen Hickman, & Samuel Radulski</h3>`;
+      description.innerHTML = `<h2>XCVB</h2> <h3>May 25th 2025</h3> <br> <p>XCVB is a roguelike deckbuilder / 2D platformer mashup originally created in for Untitled Game Jam #110. You play as a lone gambler with a deck of magical cards who ascend's Starface's tower in order to reclaim the $5 they are owed. Featuring 16 different movement cards (for active abilities), and 24 different badges (for passive perks), There are countless combinations to discover. The enemies in the tower get stronger as you climb, but on your own terms, as you get to decide if they become faster or stronger each floor. You can play XCVB for free today on its <a href="https://leaflight-studios.itch.io/xcvb">itch.io</a> page, or <a href="https://leaflight-studios.itch.io/">learn more about it here.</a></p> <br> <h3>Created By: Anthony D. Salsbury, Owen Hickman, & Samuel Radulski</h3>`;
       break;
     case 4:
-      description.innerHTML = `<h2>Mage Hand</h2> <h3>July 31st 2024</h3> <br> <p>Mage Hand is a knowledge-based exploration game originally created for Pirate Software Game Jam #15. You are a magician who has lost your memory, but retained your ability to cast spells via different hand signs. Explore out of the prison tower and find your way out of the mansion, with secret routes and legendary wizards, reclaim your memory and master some of the 40 different spells to defeat your captor and set yourself free. You can play Mage Hand for free directly from its <a href="https://leaflight-studios.itch.io/mage-hand">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury, Josh "BB" Bang, Willow Bradshaw</h3>`;
+      description.innerHTML = `<h2>Kill Protocol</h2> <h3>October 11th 2024</h3> <br> <p>Kill Protocol is a unique two-color shooter created originally for 1-BIT JAM #4. Set in a future where clones are easily accessible, lives have little meaning, you must pay back $4500 of debt to regain your freedom, and taking assasination jobs to do so. If you have good credit you can spend some on the black market to enhance your loadout. All guns in this dystopian future are linked to ID codes, and can be morphed into others, while reloading directly charges your (in game) bank account. Kill Protocol can be played with a free download from its<a href="https://leaflight-studios.itch.io/kill-protocol"> itch.io </a>page, or<a href="https://leaflight-studios.itch.io/"> learn more about it here.</a></p> <br> <h3>Created By: Anthony D. Salsbury, Jonathan Schultz, Justin Grant, George Dietritch, Owen Hickman, & Samuel Radulski</h3>`;
       break;
     case 5:
-      description.innerHTML = `<h2>goblin knight</h2> <h3>May 11th 2024</h3> <br> <p>goblin knight is an experimental 1st person sword fighting game initially created for the Game Idea Jam 2024. Play as an explorer who has gone too deep in the dark, meeting the goblin knight. It displays an interesting idea with it's aiming-based melee combat. You can download goblin knight for free from its <a href="https://leaflight-studios.itch.io/goblin-knight">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
+      description.innerHTML = `<h2>Mage Hand</h2> <h3>July 31st 2024</h3> <br> <p>Mage Hand is a knowledge-based exploration game originally created for Pirate Software Game Jam #15. You are a magician who has lost your memory, but retained your ability to cast spells via different hand signs. Explore out of the prison tower and find your way out of the mansion, with secret routes and legendary wizards, reclaim your memory and master some of the 40 different spells to defeat your captor and set yourself free. You can play Mage Hand for free directly from its <a href="https://leaflight-studios.itch.io/mage-hand">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury, Josh "BB" Bang, Willow Bradshaw</h3>`;
       break;
     case 6:
-      description.innerHTML = `<h2>Wyrm Canyon</h2> <h3>April 20th 2024</h3> <br> <p>Wyrm Canyon on the Pink and Purple Planet is an arcade-style, two-color game originally made for 1-BIT JAM #3. Pilot your sand tank with a unique control scheme and use it's compass to locate the Wyrms of the canyon, all before striking with your smylsknife on foot. If Pink and Purple isn't your style then you can pick a different color palette in the settings as well. You can play it for free directly from Wyrm Canyon's <a href="https://leaflight-studios.itch.io/wyrm-canyon">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
+      description.innerHTML = `<h2>goblin knight</h2> <h3>May 11th 2024</h3> <br> <p>goblin knight is an experimental 1st person sword fighting game initially created for the Game Idea Jam 2024. Play as an explorer who has gone too deep in the dark, meeting the goblin knight. It displays an interesting idea with it's aiming-based melee combat. You can download goblin knight for free from its <a href="https://leaflight-studios.itch.io/goblin-knight">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
       break;
     case 7:
+      description.innerHTML = `<h2>Wyrm Canyon</h2> <h3>April 20th 2024</h3> <br> <p>Wyrm Canyon on the Pink and Purple Planet is an arcade-style, two-color game originally made for 1-BIT JAM #3. Pilot your sand tank with a unique control scheme and use it's compass to locate the Wyrms of the canyon, all before striking with your smylsknife on foot. If Pink and Purple isn't your style then you can pick a different color palette in the settings as well. You can play it for free directly from Wyrm Canyon's <a href="https://leaflight-studios.itch.io/wyrm-canyon">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
+      break;
+    case 8:
       description.innerHTML = `<h2>SHAHARAZON</h2> <h3>March 14th 2024</h3> <br> <p>SHAHARAZON is a first person "grenadier" set in the dark future of 2424, originally created for Acerola Jam 0. Play through a simulation of the war of the future as a cyborg super-soldier, trained for combat in grenade-only situations. You can play SHAHARAZON for free by downloading it from its <a href="https://leaflight-studios.itch.io/agj0project">itch.io</a> page, or <a href="https://leaflight-studios.itch.io">learn more here</a></p> <br> <h3>Created By: Anthony D. Salsbury</h3>`;
       break;
     default:
